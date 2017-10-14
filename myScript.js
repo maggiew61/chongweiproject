b810212e75c48f1411545039272467536c320738
maggiew61 <9thflr.maggie@gmail.com>
2017-10-14 13:08:17 +0800

    up

--- myScript.js
+++ myScript.js
@@ -1,28 +1,25 @@
+let banner = [
+  { story: "如何瞭解蝴蝶蘭花苗產品的出口競爭和進口需求市場情報?", url: "powerbi_1.html" },
+  { story: "如何瞭解蝴蝶蘭切花產品的出口競爭和進口需求市場情報?", url: "powerbi_2.html" },
+  { story: "如何瞭解家禽產品的出口競爭和進口需求市場情報?", url: "powerbi_3.html" },
+  { story: "如何瞭解家禽產品的出口競爭和進口需求市場情報?", url: "powerbi_4.html" },
+  { story: "如何瞭解家禽產品的出口競爭和進口需求市場情報?", url: "powerbi_5.html" }
+];
+// banner.forEach(function(e){
+//   console.log(e,e.story)  //e is the value
+// })
 
-    let banner=[
-      { story: "如何瞭解蝴蝶蘭花苗產品的出口競爭和進口需求市場情報?", url: "powerbi_1.html" },
-      { story: "如何瞭解蝴蝶蘭切花產品的出口競爭和進口需求市場情報?", url: "powerbi_2.html" },
-      { story: "如何瞭解家禽產品的出口競爭和進口需求市場情報?", url: "powerbi_3.html" },
-      { story: "如何瞭解家禽產品的出口競爭和進口需求市場情報?", url: "powerbi_4.html" },
-      { story: "如何瞭解家禽產品的出口競爭和進口需求市場情報?", url: "powerbi_5.html" }
-    ];
+let str =''
 
-    // console.log(array[0].url)
+banner.forEach(function(e){
+  str+="<div id="+'text'+" class="+'carousel-item'+" ><a href=" +e.url   +"  >"+e.story+"</a></div>"
+  // str+="<div>"+e.story+"</div>"
+  console.log(e.story)
+  document.getElementById('carousel_text').innerHTML=str
+  })
 
-    // function myFunction(item, index) {
-    //     a.innerHTML = a.innerHTML + "index[" + index + "]: " + item + "<br>";
-    // }
-    // banner.forEach(function(e) {
-    //   let a=document.getElementById('demo');
-    //   a.textContent=e;
-    //   console.log(e)
-    // })
-
-    function myFunction(e){
-      let a=document.getElementById('demo');
-      // let b=JSON.stringify(e)
-      a.textContent = a.textContent+e.story+e.url;
-      console.log(e)
-    }
-    // banner.forEach(function(e) {console.log(e)})
-    // banner.forEach(myfunction(e) {console.log(e)})
+function addCass(){
+	let b=document.getElementById('text')
+	b.className +=" active"
+}
+addCass()
